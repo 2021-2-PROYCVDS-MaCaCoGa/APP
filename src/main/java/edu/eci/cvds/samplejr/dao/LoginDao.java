4e75bdf34f9133715a221be51032059276a52e6f
Diego Gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-10-26 14:16:40 -0500

    Mapper usuario

--- src/main/java/edu/eci/cvds/samplejr/dao/LoginDao.java
+++
@@ -1,7 +0,0 @@
-package edu.eci.cvds.samplejr.dao;
-
-import edu.eci.cvds.samples.entities.Usuario;
-
-public interface LoginDao {
-    public Usuario consultarUsuario() throws PersistenceException;
-}
--- src/main/java/edu/eci/cvds/samples/services/impl/ServiciosEscuelaImpl.java
+++ src/main/java/edu/eci/cvds/samples/services/impl/ServiciosEscuelaImpl.java
@@ -4,15 +4,16 @@ import javax.inject.Inject;
 
 import org.apache.ibatis.javassist.bytecode.stackmap.BasicBlock.Catch;
 
-import edu.eci.cvds.samplejr.dao.LoginDao;
+
 import edu.eci.cvds.samplejr.dao.PersistenceException;
+import edu.eci.cvds.samplejr.dao.UsuarioDao;
 import edu.eci.cvds.samples.entities.Usuario;
 import edu.eci.cvds.samples.services.ExcepcionServiciosEscuela;
 import edu.eci.cvds.samples.services.ServiciosEscuela;
 
 public class ServiciosEscuelaImpl implements ServiciosEscuela {
     @Inject
-    private LoginDao login;
+    private UsuarioDao usuario;
 
     @Override
     public boolean iniciarSesion(String login, String contraseña) throws ExcepcionServiciosEscuela {
