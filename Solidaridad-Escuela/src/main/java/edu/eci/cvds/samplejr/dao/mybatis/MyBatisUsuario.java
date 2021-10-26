4a00cd97291208b7a763dec1144c5347de8f224b
Critian Castellanos <cristian.castellanos@mail.escuelaing.edu.co>
2021-10-25 23:40:39 -0500

    Servicio de Iniciar Sesion de Usuario parcialmente implementado

---
+++ Solidaridad-Escuela/src/main/java/edu/eci/cvds/samplejr/dao/mybatis/MyBatisUsuario.java
@@ -0,0 +1,23 @@
+package edu.eci.cvds.samplejr.dao.mybatis;
+
+import com.google.inject.Inject;
+import edu.eci.cvds.samplejr.dao.PersistenceException;
+import edu.eci.cvds.samplejr.dao.UsuarioDao;
+import edu.eci.cvds.samplejr.dao.mybatis.mappers.UsuarioMapper;
+import edu.eci.cvds.samples.entities.Usuario;
+
+public class MyBatisUsuario implements UsuarioDao {
+    
+    @Inject
+    UsuarioMapper usuarioMapper;
+
+    @Override
+    public Usuario iniciarSesion(String login, String contraseña) throws PersistenceException{
+        try{
+            usuarioMapper.iniciarSesion(login, contraseña);
+        }catch(Exception e){
+            throw new PersistenceException("El usuario o contraseña estan mal escritos o son invalidos: ", e);
+        }
+    }
+}
+
