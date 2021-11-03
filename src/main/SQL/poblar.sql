029351ab8cf19a82fc9f3e84a179535b131f0e50
DiegoGonzalez2807 <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-11-03 14:46:07 -0500

    Seguridad y pruebas

---
+++ src/main/SQL/poblar.sql
@@ -0,0 +1,2 @@
+INSERT INTO USUARIO VALUES(1,'Diego Gonzalez','Estudiante','diego@gmail.com','prueba123','DiegoG');
+INSERT INTO USUARIO VALUES(2,'Cristian Castellanos','Administrador','cristian@gmail.com','prueba123','CristianC');
