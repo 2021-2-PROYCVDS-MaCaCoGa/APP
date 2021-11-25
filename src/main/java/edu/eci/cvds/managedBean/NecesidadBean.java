4c982c16c47b37379bd1a3269f91bfc649c63eb5
DiegoGonzalez2807 <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-11-25 08:16:16 -0500

    empezando necesidad

--- src/main/java/edu/eci/cvds/managedBean/NecesidadBean.java
+++ src/main/java/edu/eci/cvds/managedBean/NecesidadBean.java
@@ -20,8 +20,7 @@ public class NecesidadBean {
     private String descripcionNecesidad;
     private int urgenciaNecesidad;
     private String UsuarioNecesidad; //--> No sabria en que espacio ponerlo...
-    private String mensaje;
-    private FacesMessage.Severity estadoSolicitud;
+  
     
     
     /**
@@ -32,34 +31,12 @@ public class NecesidadBean {
     public void agregarNecesidad(){
         try{
             serviciosEscuela.expresarNecesidad(categoriaNecesidad, nombreNecesidad, descripcionNecesidad, urgenciaNecesidad,UsuarioNecesidad);
-            mensajeSinErrores();
+        
         }
         catch(Exception exception){
-            mensajeConErrores(exception.getMessage());
         }
     }
     
-    /**
-     * Se encarga de decirle al usuario que su solicitud de categoria
-     * ha pasado sin problemas.
-     */
-    public void mensajeSinErrores(){
-        this.mensaje = "Se pudo crear la categoria";
-        this.estadoSolicitud = FacesMessage.SEVERITY_INFO;
-    }
-    
-    /**
-     * Se encarga de decirle al usuario que hubo un problema
-     * con la solicitud requerida en la categoria
-     * @param mensajeError --> Mensaje enviado por la excepcion
-     */
-    public void mensajeConErrores(String mensajeError){
-        this.mensaje = mensajeError;
-        this.estadoSolicitud = FacesMessage.SEVERITY_WARN;  
-    }
-    
-    public void setNombreCategoria(){
-        
-    }
+
     
 }
